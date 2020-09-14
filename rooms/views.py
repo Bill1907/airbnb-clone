233435977d0b79a1ff7d392e814fcd49898c9351
Boseong <59524676+Bill1907@users.noreply.github.com>
2020-09-14 23:38:04 +0900

    # Detail View

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -11,3 +11,10 @@ class HomeView(ListView):
     ordering = "created"
     paginate_orphan = 5
     context_object_name = "rooms"
+
+
+class RoomDetail(DetailView):
+
+    """Room Detail Definition"""
+
+    model = models.Room
